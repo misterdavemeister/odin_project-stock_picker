adce61a36e09b2b030c63e8e5b74c45d4b702fe1
David Cole <misterdavemeister@gmail.com>
2015-10-09 17:24:57 -0500

    tweak alignment/assignment

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
 def stock_picker(arr)
-    max = 0; max_arr = []
+    max = 0; max_arr = Array.new
     arr.each_with_index do |buy_price, buy_idx|
         if buy_idx < arr.length - 1
             ((buy_idx + 1)...arr.length).each do |sell_idx|
                 prof = arr[sell_idx] - buy_price
                 if prof > max
-                max = prof
-                max_arr = [buy_idx, sell_idx]
+                    max = prof
+                    max_arr = [buy_idx, sell_idx]
                 end
             end
         end
